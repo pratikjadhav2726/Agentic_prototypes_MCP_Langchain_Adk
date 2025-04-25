a75d48815e0875e7797791f957920ce9be132ef8
Pratik Shankar Jadhav <44173994+pratikjadhav2726@users.noreply.github.com>
2025-04-24 22:51:06 -0700

    Added way to import MCP server via config file

--- Langchain_adapter_mcp.py
+++ Langchain_adapter_mcp.py
@@ -8,6 +8,7 @@ from langgraph.prebuilt import create_react_agent
 import os
 import asyncio
 from dotenv import load_dotenv
+import json
 load_dotenv()
 from langchain_groq import ChatGroq
 model = ChatGroq(
@@ -15,33 +16,22 @@ model = ChatGroq(
             groq_api_key=os.getenv("GROQ_API_KEY"), 
             model_name="qwen-qwq-32b"
         )
-
+# Load MCP server configuration from JSON file
+def load_mcp_server_config(config_path="mcp_server_config.json"):
+    try:
+        with open(config_path, "r") as config_file:
+            config = json.load(config_file)
+        return config
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Configuration file '{config_path}' not found.")
+    except json.JSONDecodeError as e:
+        raise ValueError(f"Error decoding JSON from '{config_path}': {e}")
 
 
 async def main():
+    config = load_mcp_server_config(os.getenv("MCP_Config"))
     async with MultiServerMCPClient(
-    {
-    "math": {
-            "command": "python",
-            # Make sure to update to the full absolute path to your math_server.py file
-            "args": ["math_server.py"],
-            "transport": "stdio",
-        },
-        "playwright": {
-            "command": "npx",
-            "args": ["@playwright/mcp@latest"],
-            "env": {
-                "DISPLAY": ":1"
-            }
-        },
-        "airbnb": {
-      "command": "npx",
-      "args": [
-        "-y",
-        "@openbnb/mcp-server-airbnb"
-      ]
-    }
-    }
+      config.get("mcpServers")
     )as client:
         agent = create_react_agent(model, client.get_tools())
         math_response = await agent.ainvoke({"messages": "what's (3 + 5) x 12?"})
